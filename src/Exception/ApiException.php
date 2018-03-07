2d075810af4a58de681a640c382d1a8b73302bf0
Andrew Berry <andrew.berry@lullabot.com>
2018-03-07 15:08:29 -0500

    Mark ApiException to be removed in a later PR

--- src/Exception/ApiException.php
+++ src/Exception/ApiException.php
@@ -7,6 +7,11 @@
 
 namespace Lullabot\Mpx\Exception;
 
+/**
+ * @class ApiException
+ * @package Lullabot\Mpx\Exception
+ * @deprecated Remove when all requests use other exceptions.
+ */
 class ApiException extends \Exception
 {
 }
