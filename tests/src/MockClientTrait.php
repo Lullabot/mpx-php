474beb58e53b54b64a036886f0c7655b764d387b
Andrew Berry <andrew.berry@lullabot.com>
2018-03-09 13:24:35 -0500

    Doc comments on the same line

--- tests/src/MockClientTrait.php
+++ tests/src/MockClientTrait.php
@@ -13,14 +13,13 @@ trait MockClientTrait
     /**
      * Constructs a mock Lullabot\Mpx client.
      *
-     * @param array $handler_queue
-     *                             An optional array of requests if provided, will be passed into a new
+     * @param array $handler_queue An optional array of requests if provided, will be passed into a new
      *                             \GuzzleHttp\Handler\MockHandler object an added as $options['handler'].
-     * @param array $options
-     *                             An optional array of client configuration.
+     * @param array $options An optional array of client configuration.
      *
-     * @return \Lullabot\Mpx\Client
-     *                              A configured Lullabot\Mpx client.
+     *
+     * @return \Lullabot\Mpx\Client A configured Lullabot\Mpx client.
+     *
      */
     protected function getMockClient(array $handler_queue = [], array $options = [])
     {
