f455394434b0b391a38c33f937792cf4750ac311
Andrew Berry <andrew.berry@lullabot.com>
2018-03-09 13:25:07 -0500

    Indentation fix

--- tests/src/MockClientTrait.php
+++ tests/src/MockClientTrait.php
@@ -15,11 +15,9 @@ trait MockClientTrait
      *
      * @param array $handler_queue An optional array of requests if provided, will be passed into a new
      *                             \GuzzleHttp\Handler\MockHandler object an added as $options['handler'].
-     * @param array $options An optional array of client configuration.
-     *
+     * @param array $options       An optional array of client configuration.
      *
      * @return \Lullabot\Mpx\Client A configured Lullabot\Mpx client.
-     *
      */
     protected function getMockClient(array $handler_queue = [], array $options = [])
     {
