04806e7377d6996b6aae54d38146612314772a11
Andrew Berry <andrew.berry@lullabot.com>
2018-03-13 17:20:59 -0400

    Add an initial ResolveAllUrls implementation

---
+++ src/Service/AccessManagement/ResolveAllUrls.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Lullabot\Mpx\Service\AccessManagement;
+
+use Lullabot\Mpx\Service\IdentityManagement\UserSession;
+use Psr\Http\Message\ResponseInterface;
+
+class ResolveAllUrls
+{
+    const SCHEMA_VERSION = '1.0';
+
+    const RESOLVE_ALL_URLS_URL = 'https://access.auth.theplatform.com/web/Registry/resolveAllUrls';
+
+    /**
+     * An array of resolved URLs for the service.
+     *
+     * @var array
+     */
+    protected $resolved;
+
+    /**
+     * The service these URLs correspond to, such as 'Media Data Service'.
+     *
+     * @var string
+     */
+    protected $service;
+
+    /**
+     * ResolveAllUrls constructor.
+     *
+     * @todo Is there value in storing Responses in all classes?
+     *
+     * @param string $service
+     * @param array $data
+     */
+    public function __construct(string $service, array $data) {
+        if (!isset($data['resolveAllUrlsResponse'])) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->resolved = $data['resolveAllUrlsResponse'];
+        $this->service = $service;
+    }
+
+    public static function load(UserSession $userSession, string $service)
+    {
+        $options = [
+            'query' => [
+                'schema' => static::SCHEMA_VERSION,
+                '_service' => $service,
+            ],
+        ];
+
+        return $userSession->requestAsync('GET', self::RESOLVE_ALL_URLS_URL, $options)->then(function(ResponseInterface $response) use ($service) {
+            return new static($service, \GuzzleHttp\json_decode($response->getBody(), true));
+        });
+    }
+
+    /**
+     * Return the resolved URLs for this service.
+     *
+     * @return string[]
+     */
+    public function getResolved(): array {
+        return $this->resolved;
+    }
+
+    /**
+     * Return the service these URLs correspond to, such as 'Media Data Service'.
+     *
+     * @return string
+     */
+    public function getService(): string {
+        return $this->service;
+    }
+}
