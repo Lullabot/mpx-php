6907755e22a359859fbb679434596df757101f56
Andrew Berry <andrew.berry@lullabot.com>
2018-03-22 13:48:14 -0400

    Test TokenNotFoundException

--- src/Exception/TokenNotFoundException.php
+++ src/Exception/TokenNotFoundException.php
@@ -4,8 +4,18 @@ namespace Lullabot\Mpx\Exception;
 
 use Lullabot\Mpx\User;
 
+/**
+ * Exception thrown when a token is not found in the token cache.
+ */
 class TokenNotFoundException extends \RuntimeException
 {
+    /**
+     * TokenNotFoundException constructor.
+     *
+     * @param User            $user The user the token was not found for.
+     * @param int             $code The exception code.
+     * @param \Throwable|null $previous The previous exception, if available.
+     */
     public function __construct(User $user, int $code = 0, \Throwable $previous = null)
     {
         parent::__construct(sprintf('Token not found for %s.', $user->getUsername()), $code, $previous);
