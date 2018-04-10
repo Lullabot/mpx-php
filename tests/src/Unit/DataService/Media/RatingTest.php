34994f65484dfea6d5c8de67b58c86735a6966cc
Andrew Berry <andrew.berry@lullabot.com>
2018-04-10 11:07:46 -0500

    Test Ratings

---
+++ tests/src/Unit/DataService/Media/RatingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Lullabot\Mpx\Tests\Unit\DataService\Media;
+
+use Lullabot\Mpx\DataService\DataServiceExtractor;
+use Lullabot\Mpx\DataService\Media\Rating;
+use Lullabot\Mpx\DataService\Media\Media;
+use Lullabot\Mpx\Tests\Unit\DataService\ObjectTestBase;
+
+/**
+ * Test the Rating data object.
+ *
+ * @covers \Lullabot\Mpx\DataService\Media\Rating
+ */
+class RatingTest extends ObjectTestBase
+{
+    protected $class = Rating::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $dataServiceExtractor = new DataServiceExtractor();
+        $dataServiceExtractor->setClass($this->class);
+        $this->loadFixture('ratings-object.json', $dataServiceExtractor);
+        $this->decoded = $this->decoded['ratings'][1];
+    }
+
+    /**
+     * Test get / set methods.
+     *
+     * @param string $field    The field on Player to test.
+     * @param mixed  $expected (optional) Override the assertion if data is converted, such as with timestamps.
+     *
+     * @dataProvider getSetMethods
+     */
+    public function testGetSet(string $field, $expected = null)
+    {
+        $this->assertObjectClass($this->class, $field, $expected);
+    }
+
+}
