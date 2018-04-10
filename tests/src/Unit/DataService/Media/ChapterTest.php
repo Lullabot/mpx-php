59a14d281e00361236dfca3d2662e6b26fda5c86
Andrew Berry <andrew.berry@lullabot.com>
2018-04-10 11:08:02 -0500

    Test Chapters

---
+++ tests/src/Unit/DataService/Media/ChapterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Lullabot\Mpx\Tests\Unit\DataService\Media;
+
+use Lullabot\Mpx\DataService\DataServiceExtractor;
+use Lullabot\Mpx\DataService\Media\Chapter;
+use Lullabot\Mpx\DataService\Media\Media;
+use Lullabot\Mpx\Tests\Unit\DataService\ObjectTestBase;
+
+/**
+ * Test the Chapter data object.
+ *
+ * @covers \Lullabot\Mpx\DataService\Media\Chapter
+ */
+class ChapterTest extends ObjectTestBase
+{
+    protected $class = Chapter::class;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $dataServiceExtractor = new DataServiceExtractor();
+        $dataServiceExtractor->setClass($this->class);
+        $this->loadFixture('media-object.json', $dataServiceExtractor);
+        $this->decoded = $this->decoded['chapters'][0];
+    }
+
+    /**
+     * Test get / set methods.
+     *
+     * @param string $field    The field on Player to test.
+     * @param mixed  $expected (optional) Override the assertion if data is converted, such as with timestamps.
+     *
+     * @dataProvider getSetMethods
+     */
+    public function testGetSet(string $field, $expected = null)
+    {
+        $this->assertObjectClass($this->class, $field, $expected);
+    }
+}
