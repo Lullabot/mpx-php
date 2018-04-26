703038cbf19557315c072fd748f86b1e7bef0e6f
Andrew Berry <andrew.berry@lullabot.com>
2018-04-26 10:54:06 -0400

    Fix comment sentence casing

--- src/DataService/ObjectInterface.php
+++ src/DataService/ObjectInterface.php
@@ -8,56 +8,56 @@ namespace Lullabot\Mpx\DataService;
 interface ObjectInterface
 {
     /**
-     * Returns The date and time that this object was created.
+     * Returns the date and time that this object was created.
      *
      * @return \DateTime
      */
     public function getAdded(): \DateTime;
 
     /**
-     * Set The date and time that this object was created.
+     * Set the date and time that this object was created.
      *
      * @param \DateTime
      */
     public function setAdded($added);
 
     /**
-     * Returns The id of the user that created this object.
+     * Returns the id of the user that created this object.
      *
      * @return \Psr\Http\Message\UriInterface
      */
     public function getAddedByUserId(): \Psr\Http\Message\UriInterface;
 
     /**
-     * Set The id of the user that created this object.
+     * Set the id of the user that created this object.
      *
      * @param \Psr\Http\Message\UriInterface
      */
     public function setAddedByUserId($addedByUserId);
 
     /**
-     * Returns The globally unique URI of this object.
+     * Returns the globally unique URI of this object.
      *
      * @return \Psr\Http\Message\UriInterface
      */
     public function getId(): \Psr\Http\Message\UriInterface;
 
     /**
-     * Set The globally unique URI of this object.
+     * Set the globally unique URI of this object.
      *
      * @param \Psr\Http\Message\UriInterface
      */
     public function setId($id);
 
     /**
-     * Returns The id of the account that owns this object.
+     * Returns the id of the account that owns this object.
      *
      * @return \Psr\Http\Message\UriInterface
      */
     public function getOwnerId(): \Psr\Http\Message\UriInterface;
 
     /**
-     * Set The id of the account that owns this object.
+     * Set the id of the account that owns this object.
      *
      * @param \Psr\Http\Message\UriInterface
      */
