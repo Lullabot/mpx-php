bef2b351678d38583e63b56c6e9ffbceede47cfe
Andrew Berry <andrew.berry@lullabot.com>
2018-05-01 11:28:04 -0400

    Add notification exception creation to the exception factory

--- src/Exception/MpxExceptionFactory.php
+++ src/Exception/MpxExceptionFactory.php
@@ -30,7 +30,44 @@ class MpxExceptionFactory
         MpxExceptionTrait::validateData($data);
 
         $altered = $response->withStatus($data['responseCode'], $data['title']);
+        return self::createException($request, $altered, $previous, $ctx);
+    }
 
+    /**
+     * Create a new MPX API exception from a notification.
+     *
+     * @param RequestInterface  $request
+     * @param ResponseInterface $response
+     * @param \Exception|null   $previous
+     * @param array             $ctx
+     *
+     * @return ClientException|ServerException
+     */
+    public static function createFromNotificationException(RequestInterface $request, ResponseInterface $response, \Exception $previous = null, array $ctx = [])
+    {
+        $data = \GuzzleHttp\json_decode($response->getBody(), true);
+        MpxExceptionTrait::validateNotificationData($data);
+
+        $altered = $response->withStatus($data[0]['entry']['responseCode'], $data[0]['entry']['title']);
+        return self::createException($request, $altered, $previous, $ctx);
+    }
+
+    /**
+     * Create a client or server exception.
+     *
+     * @param RequestInterface $request
+     * @param ResponseInterface $altered
+     *
+     * @param \Exception $previous
+     * @param array $ctx
+     *
+     * @return ClientException|ServerException
+     */
+    private static function createException(RequestInterface $request,
+        ResponseInterface $altered,
+        \Exception $previous = null,
+        array $ctx = []
+    ) {
         if ($altered->getStatusCode() >= 400 && $altered->getStatusCode() < 500) {
             return new ClientException($request, $altered, $previous, $ctx);
         }
