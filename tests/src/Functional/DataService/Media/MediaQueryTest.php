59a935f51c914847e49c6d062974f1efdd9012da
Andrew Berry <andrew.berry@lullabot.com>
2018-05-11 14:43:19 -0400

    Add a functional test loading the end of a list

--- tests/src/Functional/DataService/Media/MediaQueryTest.php
+++ tests/src/Functional/DataService/Media/MediaQueryTest.php
@@ -5,6 +5,7 @@ namespace Lullabot\Mpx\Tests\Functional\DataService\Media;
 use Lullabot\Mpx\DataService\ByFields;
 use Lullabot\Mpx\DataService\DataObjectFactory;
 use Lullabot\Mpx\DataService\DataServiceManager;
+use Lullabot\Mpx\DataService\ObjectList;
 use Lullabot\Mpx\DataService\Range;
 use Lullabot\Mpx\Tests\Functional\FunctionalTestBase;
 use Psr\Http\Message\UriInterface;
@@ -37,6 +38,39 @@ class MediaQueryTest extends FunctionalTestBase
             if ($index + 1 > 2) {
                 break;
             }
+        }
+    }
+
+    /**
+     * Loads the most recently uploaded media items.
+     *
+     * The fastest way to do this is to add a sort on the initial query.
+     * However, a bug was found in our range calculation when test code worked
+     * this way, which while not optimal should still work.
+     */
+    public function testLoadRecentContent()
+    {
+        $manager = DataServiceManager::basicDiscovery();
+        $dof = new DataObjectFactory($manager->getDataService('Media Data Service', 'Media', '1.10'), $this->authenticatedClient);
+        $filter = new ByFields();
+        $range = new Range();
+        $range->setStartIndex(1)
+            ->setEndIndex(2);
+        $filter->setRange($range);
+        /** @var ObjectList $list */
+        $list = $dof->selectRequest($filter, $this->account)->wait();
+
+        // Determine the end of the list of ranges.
+        $ranges = Range::nextRanges($list);
+        $filter->setRange(end($ranges));
+        $results = $dof->select($filter, $this->account);
+
+        foreach ($results as $index => $result) {
+            $this->assertInstanceOf(UriInterface::class, $result->getId());
+
+            // Loading the object by itself.
+            $reload = $dof->load($result->getId());
+            $this->assertEquals($result, $reload->wait());
         }
     }
 }
