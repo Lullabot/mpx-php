449466ecd4b3a78e436dc0abd8560f7ff5e13312
Andrew Berry <andrew.berry@lullabot.com>
2018-05-15 14:49:59 -0400

    Remove @ being detected as an annotation

--- src/DataService/CustomFieldInterface.php
+++ src/DataService/CustomFieldInterface.php
@@ -5,7 +5,7 @@ namespace Lullabot\Mpx\DataService;
 /**
  * Interface for all classes implementing a custom field definition.
  *
- * All classes implementing the @CustomField annotation must implement this
+ * All classes implementing the CustomField annotation must implement this
  * interface.
  *
  * @see \Lullabot\Mpx\DataService\Annotation\CustomField
