8b0fc1d207af2953ccda9d06c03dd3301b63e955
Andrew Berry <andrew.berry@lullabot.com>
2018-05-15 14:23:30 -0400

    Add a class for discovered custom fields

---
+++ src/DataService/DiscoveredCustomField.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lullabot\Mpx\DataService;
+
+use Lullabot\Mpx\DataService\Annotation\CustomField;
+
+/**
+ * Class representing a discovered custom field class.
+ */
+class DiscoveredCustomField
+{
+    /**
+     * The fully-qualified class name of the discovered class.
+     *
+     * @var string
+     */
+    private $class;
+
+    /**
+     * The annotation object attached to the class.
+     *
+     * @var CustomField
+     */
+    private $annotation;
+
+    /**
+     * DiscoveredCustomField constructor.
+     *
+     * @param string      $class      The fully-qualified class name of the discovered class.
+     * @param CustomField $annotation The annotation object attached to the class.
+     */
+    public function __construct(string $class, CustomField $annotation)
+    {
+        $this->class = $class;
+        $this->annotation = $annotation;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    /**
+     * @return CustomField
+     */
+    public function getAnnotation()
+    {
+        return $this->annotation;
+    }
+}
