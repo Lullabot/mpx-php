e48b066dcc0df0aa7dd8500315156ff7cc0dfdc4
Andrew Berry <andrew.berry@lullabot.com>
2018-05-16 12:59:37 -0400

    Test coverage for entries with namespaces

--- tests/src/Unit/Encoder/CJsonEncoderTest.php
+++ tests/src/Unit/Encoder/CJsonEncoderTest.php
@@ -15,6 +15,7 @@ class CJsonEncoderTest extends TestCase
      *
      * @covers ::decode()
      * @covers ::decodeCustomFields()
+     * @covers ::decodeObject()
      */
     public function testDecodeCustomFields()
     {
@@ -44,5 +45,73 @@ class CJsonEncoderTest extends TestCase
                         ],
                 ],
             ], $decoded['customFields']);
+    }
+
+    public function testDecodeEntries()
+    {
+        $data = [
+            '$xmlns' => [
+                'prefix1' => 'http://example.com/ns1',
+                'prefix2' => 'http://example.com/ns2',
+            ],
+            'entries' => [
+                [
+                    'prefix1$kittens' => 'Highly recommended',
+                    'prefix2$puppies' => 'Also worth considering',
+                ],
+                [
+                    'prefix2$puppies' => 'Highly recommended',
+                    'prefix1$kittens' => 'Also worth considering',
+                ],
+            ],
+        ];
+
+        $encoder = new CJsonEncoder();
+        $decoded = $encoder->decode(json_encode($data), 'json');
+        $this->assertEquals(
+            [
+                '$xmlns' => [
+                    'prefix1' => 'http://example.com/ns1',
+                    'prefix2' => 'http://example.com/ns2',
+                ],
+                'entries' => [
+                    0 => [
+                        'prefix1$kittens' => 'Highly recommended',
+                        'prefix2$puppies' => 'Also worth considering',
+                        'customFields' => [
+                            0 => [
+                                'namespace' => 'http://example.com/ns1',
+                                'data' => [
+                                    'kittens' => 'Highly recommended',
+                                ],
+                            ],
+                            1 => [
+                                'namespace' => 'http://example.com/ns2',
+                                'data' => [
+                                    'puppies' => 'Also worth considering',
+                                ],
+                            ],
+                        ],
+                    ],
+                    1 => [
+                        'prefix2$puppies' => 'Highly recommended',
+                        'prefix1$kittens' => 'Also worth considering',
+                        'customFields' => [
+                            0 => [
+                                'namespace' => 'http://example.com/ns1',
+                                'data' => [
+                                    'kittens' => 'Also worth considering',
+                                ],
+                            ],
+                            1 => [
+                                'namespace' => 'http://example.com/ns2',
+                                'data' => [
+                                    'puppies' => 'Highly recommended',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ], $decoded);
     }
 }
