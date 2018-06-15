e03c57b831a4cb8bf83b0391b83cebad4aa0666c
Andrew Berry <andrew.berry@lullabot.com>
2018-06-15 14:21:35 -0400

    Add a Link class

---
+++ src/DataService/DataType/Link.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Lullabot\Mpx\DataService\DataType;
+
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Represents a URL along with a target, title, and mime type.
+ *
+ * @see https://docs.theplatform.com/help/wsf-index-of-supported-data-types#tp-toc7
+ */
+class Link
+{
+    /**
+     * @var UriInterface
+     */
+    protected $href;
+
+    /**
+     * @var string
+     */
+    protected $target;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @return UriInterface
+     */
+    public function getHref(): UriInterface
+    {
+        return $this->href;
+    }
+
+    /**
+     * @param UriInterface $href
+     */
+    public function setHref(UriInterface $href)
+    {
+        $this->href = $href;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTarget(): string
+    {
+        return $this->target;
+    }
+
+    /**
+     * @param string $target
+     */
+    public function setTarget(string $target)
+    {
+        $this->target = $target;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType(string $type)
+    {
+        $this->type = $type;
+    }
+}
