09dbe8b6a3d1b4d37afa04c514c3289555a1dd1f
Andrew Berry <andrew.berry@lullabot.com>
2018-07-03 08:28:14 -0400

    Add CustomField stub classes

---
+++ tests/src/Unit/DataService/CustomField/NeverUsedCustomField.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lullabot\Mpx\Tests\Unit\DataService\CustomField;
+
+use Lullabot\Mpx\DataService\Annotation\CustomField;
+use Lullabot\Mpx\DataService\CustomFieldInterface;
+
+/**
+ * Class NeverUsedCustomField.
+ *
+ * @CustomField(
+ *     namespace="http://www.example.com/never-used",
+ *     service="Media Data Service",
+ *     objectType="Media",
+ * )
+ */
+class NeverUsedCustomField implements CustomFieldInterface
+{
+    /**
+     * @var string
+     */
+    protected $neverUsed;
+
+    /**
+     * @return string
+     */
+    public function getNeverUsed(): ?string
+    {
+        return $this->neverUsed;
+    }
+
+    /**
+     * @param string $neverUsed
+     */
+    public function setNeverUsed(?string $neverUsed): void
+    {
+        $this->neverUsed = $neverUsed;
+    }
+}
