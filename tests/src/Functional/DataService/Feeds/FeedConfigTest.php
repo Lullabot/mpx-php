7705d1e72bf508dcf2d47b2b55e977bd54ec57ed
Andrew Berry <andrew.berry@lullabot.com>
2018-07-18 14:17:44 -0400

    Add a functional test for feedconfig objects

--- tests/src/Functional/DataService/Feeds/FeedConfigTest.php
+++ tests/src/Functional/DataService/Feeds/FeedConfigTest.php
@@ -2,10 +2,36 @@
 
 namespace Lullabot\Mpx\Tests\Functional\DataService\Feeds;
 
-use Lullabot\Mpx\DataService\Feeds\FeedConfig;
-use PHPUnit\Framework\TestCase;
+use Lullabot\Mpx\DataService\DataObjectFactory;
+use Lullabot\Mpx\DataService\DataServiceManager;
+use Lullabot\Mpx\DataService\ObjectListQuery;
+use Lullabot\Mpx\Tests\Functional\FunctionalTestBase;
+use Psr\Http\Message\UriInterface;
 
-class FeedConfigTest extends TestCase
+/**
+ * Tests loading feed configuration objects.
+ */
+class FeedConfigTest extends FunctionalTestBase
 {
 
+    /**
+     * Tests loading two FeedConfig objects.
+     */
+    public function testQueryFeeds()
+    {
+        $manager = DataServiceManager::basicDiscovery();
+        $service = $manager->getDataService('Feeds Data Service', 'FeedConfig', '2.2');
+        $dof = new DataObjectFactory($service, $this->authenticatedClient);
+        $filter = new ObjectListQuery();
+        $filter->getRange()->setStartIndex(1)
+            ->setEndIndex(2);
+        $results = $dof->select($filter, $this->account);
+
+        foreach ($results as $index => $result) {
+            $this->assertInstanceOf(UriInterface::class, $result->getId());
+            if ($index + 1 > 2) {
+                break;
+            }
+        }
+    }
 }
