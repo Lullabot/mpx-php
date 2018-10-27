87df2a3f5f3818dea251a018b9a3296ad1a36d6e
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2018-10-27 01:05:31 -0400

    created a new query option to limit what fields are returned

---
+++ src/DataService/QQuery/Fields.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Lullabot\Mpx\DataService\QQuery;
+
+use Lullabot\Mpx\DataService\QueryPartsInterface;
+
+/**
+ * Class for 'fields' filters on requests.
+ *
+ * @see https://docs.theplatform.com/help/wsf-selecting-objects-by-using-a-byfield-query-parameter
+ */
+class Fields implements QueryPartsInterface
+{
+    /**
+     * The array of fields to be returned in the result.
+     *
+     * @var array
+     */
+    protected $fields = [];
+
+    /**
+     * Add a field to this filter, such as 'title'.
+     *
+     * @param string $field The field to filter on.
+     *
+     * @return self Fluent return.
+     */
+    public function addField(string $field): self
+    {
+        $this->fields[] = $field;
+
+        return $this;
+    }
+
+    /**
+     * Return all of the fields being filtered.
+     *
+     * @return array
+     */
+    public function toQueryParts(): array
+    {
+        $fields = [];
+
+        $fields['fields'] = implode(',', $this->fields);
+
+        return $fields;
+    }
+}
