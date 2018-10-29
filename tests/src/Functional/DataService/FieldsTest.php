78edb8a5ef7a047d589885391c8f26b2aef14b1c
Andrew Berry <andrew.berry@lullabot.com>
2018-10-29 12:45:14 -0400

    Add a functional test showing limited field returns

---
+++ tests/src/Functional/DataService/FieldsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lullabot\Mpx\Tests\Functional\DataService;
+
+use Lullabot\Mpx\DataService\DataObjectFactory;
+use Lullabot\Mpx\DataService\DataServiceManager;
+use Lullabot\Mpx\DataService\Fields;
+use Lullabot\Mpx\DataService\ObjectListQuery;
+use Lullabot\Mpx\DataService\Range;
+use Lullabot\Mpx\Tests\Functional\FunctionalTestBase;
+
+class FieldsTest extends FunctionalTestBase
+{
+
+    public function testLimitFields()
+    {
+        $manager = DataServiceManager::basicDiscovery();
+        $mediaDataService = $manager->getDataService('Media Data Service', 'Media', '1.10');
+        $dof = new DataObjectFactory($mediaDataService->getAnnotation()->getFieldDataService(), $this->authenticatedClient);
+        $filter = new ObjectListQuery();
+        $fields = (new Fields())->addField('title');
+        $filter->add($fields);
+        $filter->setRange((new Range())->setStartIndex(1)->setEndIndex(1));
+        $results = $dof->select($filter, $this->account);
+        $results->valid();
+        $result = $results->current();
+        $json = $result->getJson();
+        $this->assertCount(1, $json);
+        $this->assertArrayHasKey('title', $json);
+    }
+}
